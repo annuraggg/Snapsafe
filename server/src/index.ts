236722e983527d3f1ea7ba0eb6effc25f74fb544
annuraggg <anuragsawant@duck.com>
2024-05-14 18:19:23 +0530

    chore: Add health check endpoint and _redirects

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,6 +5,13 @@ import files from "./api/files/main.js";
 app.use("/auth", auth);
 app.use("/files", files);
 
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    version: process.env.VERSION,
+    health: "healthy",
+  });
+});
+
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
